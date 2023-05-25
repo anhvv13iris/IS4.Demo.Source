eab6b5925b894b81a17fda612c3368219dad6e38
anhvv <anhvv@irismedia.vn>
2023-05-25 11:38:50 +0700

    first commit

---
+++ src/AuthServer/Config.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                        //new IdentityResources.OpenId(),
+                        //new IdentityResources.Profile()
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                //new ApiScope("report.cms"),
+                //new ApiScope("report.webKH"),
+                //new ApiScope("workerservice"),
+            };
+
+        public static IEnumerable<ApiResource> GetApis => 
+         new ApiResource[]
+            {
+                //new ApiResource("report.cms")
+                //{
+                //    ApiSecrets =
+                //    {
+                //        new Secret("reportCMSSecret".Sha256())
+                //    },
+                //    Scopes = new List<string> { "report.cms" }
+                //},
+                //new ApiResource("report.webKH", "Report Web KH Service"),
+                //new ApiResource("workerservice", "Worker Service")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                //new Client
+                //{
+                //    ClientId = "m2m.client",
+                //    ClientName = "Client Credentials Client",
+
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                //    AllowedScopes = { "scope1" }
+                //},
+
+                //// interactive client using code flow + pkce
+                //new Client
+                //{
+                //    ClientId = "interactive",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    AllowedGrantTypes = GrantTypes.Code,
+
+                //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "openid", "profile", "scope2" }
+                //},
+                //new Client
+                //{
+                //    ClientName = "ReactJS Client",
+                //    ClientId = "report_cms_client_id",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    AllowedGrantTypes = GrantTypes.Code,
+
+                //    RedirectUris = { "http://localhost:3004/signin-callback" },
+                //    RequirePkce = true,
+                //    PostLogoutRedirectUris = { "http://localhost:3004/signout-callback" },
+                //    AllowedCorsOrigins = {"http://localhost:3004"},
+                //    AllowOfflineAccess = true,
+                //    AllowAccessTokensViaBrowser = true,
+                //    AllowedScopes = { "openid", "profile", "report.cms" },
+                //    RequireClientSecret = false,
+                //    RequireConsent = false,
+                //    AccessTokenLifetime = 600,
+                //    AccessTokenType = AccessTokenType.Reference
+                //},
+                //new Client
+                //{
+                //    ClientName = "ReactJS Client",
+                //    ClientId = "report_webKH_client_id",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    AllowedGrantTypes = GrantTypes.Code,
+
+                //    RedirectUris = { "http://localhost:3005/signin-callback" },
+                //    RequirePkce = true,
+                //    PostLogoutRedirectUris = { "http://localhost:3005/signout-callback" },
+                //    AllowedCorsOrigins = {"http://localhost:3005"},
+                //    AllowOfflineAccess = true,
+                //    AllowAccessTokensViaBrowser = true,
+                //    AllowedScopes = { "openid", "profile", "report.webKH" },
+                //    RequireClientSecret = false,
+                //    RequireConsent = false,
+                //    AccessTokenLifetime = 600
+                //},
+                //new Client
+                //{
+                //    ClientName = "Worker Service Demo",
+                //    ClientId = "worker_service_demo_client_id",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    AllowedScopes = { "workerservice" },
+                //},
+            };
+    }
+}
---
+++ src/AuthServer/Startup.cs
@@ -0,0 +1,174 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using AuthServer.ProfileService;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Shared.DbContexts;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Shared.Entities.Identity;
+
+namespace AuthServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<AdminIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("AuthServer")));
+
+            services.AddIdentity<UserIdentity, UserIdentityRole>()
+                .AddEntityFrameworkStores<AdminIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            //services.AddSession(options => options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.None);
+
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+                options.Authentication.CookieLifetime = TimeSpan.FromDays(30);
+                options.Authentication.CookieSlidingExpiration = true;
+            })
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryClients(Config.Clients)
+                //.AddInMemoryApiResources(Config.GetApis)
+                .AddAspNetIdentity<UserIdentity>()
+                .AddProfileService<IdentityWithAdditionalClaimsProfileService>();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
+                options.SlidingExpiration = false;
+            });
+
+            // not recommended for production - you need to store your key material somewhere secure
+            //builder.AddDeveloperSigningCredential();
+            builder.AddSigningCredential(new X509Certificate2("cert_rsa512.pfx", "123456aA@"));
+
+            services.AddAuthentication();
+            //    .AddGoogle(options =>
+            //    {
+            //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+            //        // register your IdentityServer with Google at https://console.developers.google.com
+            //        // enable the Google+ API
+            //        // set the redirect URI to https://localhost:5001/signin-google
+            //        options.ClientId = "copy client ID from Google here";
+            //        options.ClientSecret = "copy client secret from Google here";
+            //    });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
+            }
+            InitializeDatabase(app);
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
